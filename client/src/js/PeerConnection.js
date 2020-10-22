57e2c29e3c4e8235ede0f1dff1759fe4268d1351
v.skrygun <v.skrygun@geniusee.com>
2020-10-22 19:39:33 +0300

    Upd: add turn server

--- client/src/js/PeerConnection.js
+++ client/src/js/PeerConnection.js
@@ -2,7 +2,21 @@ import MediaDevice from './MediaDevice';
 import Emitter from './Emitter';
 import socket from './socket';
 
-const PC_CONFIG = { 'iceServers': [{url:'stun:stun.l.google.com:19302'} ] };
+const PC_CONFIG = { 'iceServers': [
+  {
+    url:'stun:stun.l.google.com:19302'
+  },
+  {
+    url: 'turn:192.158.29.39:3478?transport=tcp',
+    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    username: '28224511:1379330808'
+},
+{
+    url: 'turn:turn.bistri.com:80',
+    credential: 'homeo',
+    username: 'homeo'
+ },
+] };
 
 class PeerConnection extends Emitter {
   /**
